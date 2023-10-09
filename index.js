6f214a889837304e0a0994ec1429ed2c415d3761
Edgar Baudry <edgar.baudry@gmail.com>
2023-10-09 15:34:17 -0700

    changed config

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Serve the index.html file
 app.get('/', (req, res) => {
