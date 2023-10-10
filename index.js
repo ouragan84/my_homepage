46ee8ccf69372962214eb6fdd788df73ac966be4
Edgar Baudry <edgar.baudry@gmail.com>
2023-10-10 04:13:32 +0000

    added icon to website

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -8,11 +7,6 @@ const app = express();
 
 // Serve static files from the public directory
 app.use(express.static('public'));
-
-// Serve the index.html file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 // Get the port from the environment variable
 const port = process.env.PORT || 3000;
